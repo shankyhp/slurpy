76a7c3e27f3b51ef38a18d9259a5b113d8b77a39
shankyhp <birajdarhpshankar@gmail.com>
2025-01-11 03:49:45 +0530

    Final Commit Site Complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { repositoryName } from "@/prismicio";
 import "./app.css";
 import Header from "@/components/Header";
 import ViewCanvas from "@/components/ViewCanvas";
+import Footer from "@/components/Footer";
 
 const alpino = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2",
@@ -28,6 +29,7 @@ export default function RootLayout({
 
           <ViewCanvas />
         </main>
+        <Footer />
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
--- src/slices/BigText/index.tsx
+++ src/slices/BigText/index.tsx
@@ -14,8 +14,17 @@ const BigText = ({ slice }: BigTextProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="min-h-screen w-screen overflow-hidden bg-[#FE6334] text-[#FEE832]"
     >
-      Placeholder component for big_text (variation: {slice.variation}) Slices
+      <h2 className="grid w-full gap-[3vw] py-10 text-center font-black uppercase leading-[.7]">
+        <div className="text-[34vw]">Soda</div>
+        <div className="grid gap-[3vw] text-[34vw] md:flex md:text-[11vw]">
+          <span className="inline-block">that </span>
+          <span className="inline-block max-md:text-[27vw]">makes </span>
+          <span className="inline-block max-md:text-[40vw]">you </span>
+        </div>
+        <div className="text-[32vw]">Smile</div>
+      </h2>
     </section>
   );
 };
